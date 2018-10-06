110ee3aa36a7605cba565cb037b455f94629be3f
mchi168 <michaelchi168@gmail.com>
2018-10-06 14:44:24 -0700

    fix routes

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ const express = require('express');
 var cors = require('cors');
 let app = express();
 const bodyParser = require('body-parser');
+
+
 let getRestaurantsById = require('../database/index.js').getRestaurantsById;
 let getRestaurantsByName = require('../database/index.js').getRestaurantsByName;
+
+let postResaurantsById = require('../database/index.js').getRestaurantsById;
+let postResaurantsByName = require('../database/index.js').getRestaurantsByName;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,7 +34,6 @@ app.get('/:id/res', cors(corsOptions), function (req, res) {
   }
 });
 
-
 app.get('/api/header/:id/res', cors(corsOptions), function (req, res) {
   let resIdOrName = req.param('id');
   if (isNaN(parseInt(resIdOrName))) {
@@ -41,6 +46,23 @@ app.get('/api/header/:id/res', cors(corsOptions), function (req, res) {
     });
   }
 });
+
+app.post('/api/header/:id/res', (req, res) => {
+  const resIdOrName = req.param('id');
+  if (Number.isNaN(parseInt(resIdOrName, 10))) {
+    postRestaurantsByName(resIdOrName, (err, data) => {
+      res.send(JSON.stringify(data[0]));
+    });
+  } else {
+    postRestaurantsById(resIdOrName, (err, data) => {
+      res.send(JSON.stringify(data[0]));
+    });
+  }
+});
+
+
+
+
 
 
 let port = 7763;
